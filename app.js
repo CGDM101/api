3ce650a39053aa43009715d75e20e7a7292c6120
CGDM101 <cagudm@hotmail.com>
2023-11-23 20:01:44 +0100

    Added more quotes.

--- app.js
+++ app.js
@@ -86,6 +86,46 @@ app.get("/", (req, res) => {
             id: 20,
             text: "I'll meet you by the river, or maybe on the other side",
         },
+        {
+            id: 21,
+            text: "You've got to work hard if you want anything at all",
+        },
+        {
+            id: 22,
+            text: "Open the window and out go ideals",
+        },
+        {
+            id: 23,
+            text: "Show some humility,  you have the ability",
+        },
+        {
+            id: 24,
+            text: "Don't waste your energy, making apologies",
+        },
+        {
+            id: 25,
+            text: "Take one more look at what you found old, and in it you'll find something new",
+        },
+        {
+            id: 26,
+            text: "I'm shying from the light, I've always loved the night, and now you offer me eternal darkness",
+        },
+        {
+            id: 27,
+            text: "Lead me into your darkness, when this world is trying its hardest to leave me unimpressed",
+        },
+        {
+            id: 28,
+            text: "Why do you smile the smile you do?",
+        },
+        {
+            id: 29,
+            text: "Everybody's looking for a reason to live",
+        },
+        {
+            id: 30,
+            text: "Give me something to get excited about",
+        }
 
     ]
     res.set({
